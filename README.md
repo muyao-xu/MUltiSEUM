0301358615cfe61071a1341daecd52f7cccc76df
Muyao <muyaoxu@usc.edu>
2018-10-13 02:26:59 -0700

    refactor

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 for each shell session run the following command
 `export GOOGLE_APPLICATION_CREDENTIALS="/Users/muyaoxu/Desktop/MUltiSEUM/MUltiSEUM-5e4cd1401e76.json"`
+
+- to start the server
+`npm start`
+
+- to start nodemon
+`npx nodemon <filename>`
---
+++ backend/wiki.js
@@ -0,0 +1,38 @@
+const axios = require('axios');
+
+function getQueryName(name) {
+  name = encodeURI(name);
+  const URL = `https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=${name}&limit=1`;
+  return new Promise((resolve, reject) => {
+    axios.get(URL)
+      .then((response) => {
+        var returnURL = response.data[3][0];
+        var arr = returnURL.split('/');
+        console.log(URL) ;
+        console.log(arr[arr.length-1]);
+        resolve(arr[arr.length-1]);
+      })
+      .catch((err) => {
+        console.log(err);
+        reject(err);
+      });
+  });
+};
+
+function getQueryLanguage(encodedTitle, language) {
+  const URL = `https://en.wikipedia.org/w/api.php?action=query&format=json&prop=langlinks&titles=${encodedTitle}&utf8=1&llprop=url&lllang=${language}`;
+  return new Promise((resolve, reject) => {
+    axios.get(URL)
+      .then((response) => {
+        var pages = response.data.query.pages;
+        var key = Object.keys(pages)[0];
+        var langlinks = pages.key.langlinks;
+        var translate = langlinks."/*";
+        console.log(translate);
+      });
+  });
+};
+
+getQueryLanguage("Sunflowers_(Van_Gogh_series)", "zh");
+
+module.exports={getQueryName};
